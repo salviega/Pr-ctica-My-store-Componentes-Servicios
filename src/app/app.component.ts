5ccc16ca3706af73d257de04d3434e10f7413a64
Santiago Viana Vera <salviega6@gmail.com>
2022-08-03 15:03:20 -0500

    [FEAT] Ciclo de vida de componentes
    OnChange, OnInit, OnAfterViewInit, OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // assets/images/photo.jpeg
   photo: string = '';
+  disable: boolean = true;
+
+  products: Product[] = [{
+    id: '1',
+    name: 'product 1',
+    image: '../../../assets/images/defaultPhoto.png',
+    price: '100'
+  },
+  {
+    id: '2',
+    name: 'product 1',
+    image: '../../../assets/images/defaultPhoto.png',
+    price: '100'
+  },
+  {
+    id: '3',
+    name: 'product 1',
+    image: '../../../assets/images/defaultPhoto.png',
+    price: '100'
+  },
+  {
+    id: '4',
+    name: 'product 1',
+    image: '../../../assets/images/defaultPhoto.png',
+    price: '100'
+  }]
 
   onLoad(img: string) {
     console.log('Llego la respuesta del componente hijo', img)
+  }
+
+  onDisable() {
+    if (this.disable) this.disable = false;
+    else this.disable = true;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, AfterViewInit, OnDestroy, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,14 +7,51 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
-  @Input() photo: string = '';
+  photo: string = '';
+  @Input('photo') 
+  set changeImg(newImg: string) {
+    this.photo = newImg;
+    console.log('change just image')
+    // code
+  }
   @Output() loaded: EventEmitter<string> = new EventEmitter<string>();
+  counter = 0;
 
   defaultPhoto: string = 'assets/images/defaultPhoto.png';
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    // before render
+    // don't async
+    console.log('constructor', 'imgValue =>', this.photo)
   }
+
+  ngOnChanges() {
+    //before render
+    //change inputs --times
+    console.log('OnChanges', 'imgValue =>', this.photo)
+
+  }
+  ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.photo)
+    this.counter = window.setInterval(() => {
+      this.counter +1;
+      console.log('run counter')
+    }, 1000);
+  }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit', 'imgValue =>', this.photo)
+  }
+
+  ngOnDestroy(): void {
+    // delete component
+    console.log('OnDestroy', 'imgValue =>', this.photo)
+    window.clearInterval(this.counter)
+  }
+
 
   catchError() {
     this.photo = this.defaultPhoto;
